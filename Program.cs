7659ace95924a7eb092cb00c453d6b5115648ff7
Alex Grenier <greni036@umn.edu>
2023-06-23 20:06:16 -0500

    formatting

--- Program.cs
+++ Program.cs
@@ -7,20 +7,24 @@ using System.Text;
     The purpose of this Hash function is to intake strings and salt and create hash values.
     I used SHA512 because it is less prone to collision attacks and more rounds of operations at little cost.
     I did this in C# because I have no experience with it and want to learn some basics.
+    This is all done without a DB as I do not have one.
 */
 public class HashFunction {
     public static string Hashy(string input, string salt) {
+
         // Convert the salt and input string to byte arrays
         byte[] saltBytes = Encoding.UTF8.GetBytes(salt);
         byte[] inputBytes = Encoding.UTF8.GetBytes(input);
 
         // Create a new instance of the Rfc2898DeriveBytes class to generate a secure key from the salt and password
         using (var deriveBytes = new Rfc2898DeriveBytes(inputBytes, saltBytes, 10000, HashAlgorithmName.SHA512)) {
+
             // Generate the key
             byte[] keyBytes = deriveBytes.GetBytes(64); // 64 bytes for 512-bit key
 
             // Create a new instance of the HMACSHA512 class with the generated key
             using (var hmac = new HMACSHA512(keyBytes)) {
+            
                 // Compute the hash of the input string
                 byte[] hashBytes = hmac.ComputeHash(inputBytes);
 
@@ -29,6 +33,7 @@ public class HashFunction {
 
                 // Loop through each byte of the hashed data and format it as a hexadecimal string
                 for (int i = 0; i < hashBytes.Length; i++) {
+            
                     // Append each byte as a two-digit hexadecimal representation
                     stringBuilder.Append(hashBytes[i].ToString("x2"));
                 }
